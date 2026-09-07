2cd3f49a1b4e3ca2d526a5e2983d7da7cfeb6ddc
LuanClaiver <luanclaivermpp@gmail.com>
2026-09-06 23:13:13 -0300

    Atlas v1.0.7

---
+++ Iniciar Aplicacao.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal EnableExtensions
+title Atlas v1.0.7
+cd /d "%~dp0"
+
+set "APP_FILE=%~dp0Arquivos principais\www\index.html"
+
+if not exist "%APP_FILE%" (
+  echo ERRO: O arquivo principal do Atlas nao foi encontrado.
+  echo Mantenha este BAT na raiz do repositorio, ao lado da pasta Arquivos principais.
+  pause
+  exit /b 1
+)
+
+start "" "%APP_FILE%"
+endlocal
+exit /b 0
